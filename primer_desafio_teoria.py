11af976abcead5ef7372054e74771f8ffae6098c
alumno <david.camino@live.com.ar>
2021-03-22 15:54:42 -0300

    Creacion del primer desafio de teoria

---
+++ primer_desafio_teoria.py
@@ -0,0 +1,18 @@
+
+
+#Primer desafio
+numero = int(input('Ingrese un numero entero desde teclado: '))
+
+if numero % 2 == 0:
+    print("El numero {} , es par".format(numero))
+else:
+    print(f"El numero {numero} , no es par")
+
+
+
+    
+    
+    
+
+    
+    
